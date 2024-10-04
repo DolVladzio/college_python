c7ba9751135c2854f760dab6612d066f07468b41
Vladzio Dolynka <dolynkavladzio@gmail.com>
2024-10-04 15:45:01 +0300

    class Library

--- 3_oop/main.py
+++ 3_oop/main.py
@@ -45,3 +45,39 @@ class BorrowAble(abc.ABC):
     #_Abstract method-2
     def returnBook(self): pass
 #=========================================================
+#_Task_5
+class Library(BorrowAble):
+    #_List of books
+    books_list = [
+        "Surgeon",
+        "Assistant",
+        "Sinful",
+        "Double",
+        "Deathly",
+        "Mephisto Club",
+        "Keepers of Death",
+        "Killing cold",
+        "The girl who is silent",
+        "The Last One to Die",
+        "Die Again",
+        "I know a secret",
+        "Listen to me"
+    ]
+    #_Constructor-1
+    def __init__(self, nameOfBook):
+        self.nameOfBook = nameOfBook
+    #_Method-1
+    def addBook(self): return f"- The book '{self.nameOfBook}' has just added"
+    #_Destructor-2
+    def __del__(self): return f"- The book '{self.nameOfBook}' has just removed\n- Error 12596: 'You don't have a permission'\n"
+    #_Method-2(Displaying the random book from 'library')
+    def borrowBook(self):
+        #_Choosing a random number of book
+        numberOfBook = self.books_list[(random.randint(1, len(self.books_list))) - 1]
+        return f"- The book '{numberOfBook}' has already borrowed"
+    #_Method-3(Displaying the random book from 'library')
+    def returnBook(self)
+        #_Choosing a random number of book
+        numberOfBook = self.books_list[(random.randint(1, len(self.books_list))) - 1]
+        return f"- The book '{numberOfBook}' has already returned"
+#=========================================================
