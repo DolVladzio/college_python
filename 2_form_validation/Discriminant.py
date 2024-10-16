148a9cabf36877d18337644a538cb7e8d790027f
Vladzio <130439029+DolVladzio@users.noreply.github.com>
2024-10-16 19:09:19 +0300

    Update Discriminant.py

--- 2_form_validation/Discriminant.py
+++ 2_form_validation/Discriminant.py
@@ -1,4 +1,4 @@
-﻿#========================================
+#========================================
 from math import sqrt
 
 def Line():
@@ -10,51 +10,58 @@ class Discriminant:
         self.b = b
         self.c = c
         self.d = d
-
+#========================================
     def CalculateDiscriminant(self):
         try:
-            self.a = int(self.a)
-            self.b = int(self.b)
-            self.c = int(self.c)
-            self.d = int(self.d)
+            self.a = float(self.a)
+            self.b = float(self.b)
+            self.c = float(self.c)
+            self.d = float(self.d)
 
-            DiscriminantResult = (self.b ** 2) - 4 * (self.a) * (self.c)
-
-            # Line()
-            # print(f"- Example: {self.a}x^2 {self.b}x {self.c} = {self.d}")
-
-            if DiscriminantResult < 0:
-                print("- Discriminant can't be lesser than 0")
-                return "- No result"
-            elif DiscriminantResult == 0:
-                Line
-                DiscriminantResult = (- (self.b) / 2 * (self.a))
-
-                print("- Formula: - (b / 2a)")
-                return f"- Result of x: {DiscriminantResult}"
-            else:
-                Line()
-                print("- Formula: b^2 - 4ac")
+            Line()
+            self.discriminant_value = (self.b ** 2) - 4 * (self.a) * (self.c)
+            print("- Formula: b^2 - 4ac")
             
-                print(f"- D = ({self.b})^2 - 4 * {self.a} * {self.c}")
+            print(f"- D = ({self.b})^2 - 4 * ({self.a}) * ({self.c})")
+            return f"- D = √{self.discriminant_value}"
 
-                print(f"- D = √{DiscriminantResult}")
-                Line()
-
-                x1 = round(((self.b) - sqrt(DiscriminantResult)) / 2 * (self.a), 2)
-                x2 = round(((self.b) + sqrt(DiscriminantResult)) / 2 * (self.a), 2)
-
-                print(f"- Formula(x1) = ({self.b} - √{DiscriminantResult}) / (2 * {self.a})")
-                print(f"- Formula(x2) = ({self.b} + √{DiscriminantResult}) / (2 * {self.a})")
-                Line()
-
-                print( f"- x1 = {x1}\n- x2 = {x2}")
-                
-                return "--------------------------------"
         except:
             return "- Incorrect input"
+#========================================
+    def DiscriminantResult(self):
+        if self.discriminant_value < 0:
+            Line()
+
+            print("- No result\n- Discriminant can't be lesser than 0")
+
+            return "--------------------------------"
+        elif self.discriminant_value == 0:
+            Line()
+            result = -self.b / (2 * self.a)
+            
+            print("- Formula: -b / 2a")
+
+            print(f"- ({-self.b}) / (2 * {self.a})")
+            
+            print(f"- One root: x = {result}")
+
+            return "--------------------------------"
+        else:            
+            Line()
+
+            x1 = round((-self.b - sqrt(self.discriminant_value)) / (2 * self.a), 2)
+            x2 = round((-self.b + sqrt(self.discriminant_value)) / (2 * self.a), 2)
+
+            print(f"- Formula(x1) = ({self.b} - √{self.discriminant_value}) / (2 * {self.a})")
+            print(f"- Formula(x2) = ({self.b} + √{self.discriminant_value}) / (2 * {self.a})")
+            Line()
+
+            print(f"- x1 = {x1}\n- x2 = {x2}")
+            
+            return "--------------------------------"
 #_Objects_===============================
-first_object = Discriminant(-12, -5.5, 4, 0)
+first_object = Discriminant(12, 1, 4, 0)
 #_Result_================================
 print(first_object.CalculateDiscriminant())
+print(first_object.DiscriminantResult())
 #========================================
